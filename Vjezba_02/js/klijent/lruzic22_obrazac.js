ab954bf2322629950a56823b25d590a1b7fc018d
Luka <l.ruzic47@gmail.com>
2024-05-30 12:04:53 +0200

    Finished form validations using javascript

--- Vjezba_02/js/klijent/lruzic22_obrazac.js
+++ Vjezba_02/js/klijent/lruzic22_obrazac.js
@@ -1,10 +1,6 @@
 // Sadrži sve elemente oko validacije obrasca
 
-// kod datuma nemože se odabrati datum najranije 2 dana u prošlosti i najkasnije 1 mjesec u prošlosti
-
 // kod padajućeg izbornika traži se obavezno odabir barem jedne opcije iz svake grupe opcija
-
-// kod numeričkog polja traži se unos broja između 1 i 100
 
 // kod textarea koristeći regex treba se provjeriti sljedeće: prvo slovo svake rečenice mora biti veliko, a zadnji znak mora biti točka
 // U tekstu se mogu nalaziti max 4 rečenice, svaka mora započeti velikim slovom i završavati točkom, upitnikom ili uskličnikom
@@ -19,16 +15,20 @@ form = form[0];
 // dohvati gumb koji u sebi ima atribut type="submit"
 
 var inputs = document.querySelectorAll("input");
-var textareas = document.querySelectorAll("textarea");
-var selects = document.querySelectorAll("select");
+var textareas = document.querySelector("textarea");
+var selects = document.querySelector("select");
+
+console.log("dohvaćeni elementi");
 
 function isFormEmpty(event){
+    console.log("pozvana funkcija");
     // dohvaćamo sve input, textarea i select elemente
     let isGood = true;
     poruka = "";
     
     for (let i = 1; i < inputs.length; i++) {
-        if((inputs[i].type === "text" || inputs[i].type === "email") && inputs[i].value === ""){
+        if((inputs[i].type === "text" || inputs[i].type === "email" || inputs[i].type === "file") && inputs[i].value === ""){
+            console.log("prazno polje1");
             inputs[i].style.borderColor = "red";
             event.preventDefault();
             poruka = "Polje ne smije biti prazno";
@@ -37,7 +37,9 @@ function isFormEmpty(event){
             isGood = false;
             return isGood;
         }
+
         if(inputs[i].type === "number" && (inputs[i].value === "" || inputs[i].value < 1 || inputs[i].value > 100)){
+            console.log("prazno polje2");
             inputs[i].style.borderColor = "red";
             event.preventDefault();
             poruka = "Unesite broj između 1 i 100";
@@ -52,34 +54,76 @@ function isFormEmpty(event){
             // potrebno je pretvoriti date u cijeli broj jer se date.getTime() vraća u milisekundama
             let diff = Math.floor((date2.getTime() - date.getTime()) / (1000 * 60 * 60 * 24)) + 1;
             // provjeravamo je li datum najranije 2 dana u prošlosti i najkasnije 1 mjesec u prošlosti
-            if(diff < -2 || diff > -30){
+            if (diff > -2 || diff < -30 || isNaN(diff)) {
                 inputs[i].style.borderColor = "red";
                 event.preventDefault();
                 poruka = "Datum mora biti najranije 2 dana u prošlosti i najkasnije 1 mjesec u prošlosti";
                 focusEmptyForm(poruka, inputs[i]);
                 isGood = false;
                 return isGood;
-            }
+            } 
         }
     }
-    for (let i = 0; i < textareas.length; i++) {
-        if (textareas[i].value === "") {
-            textareas[i].style.borderColor = "red";
-            isGood = false;
-        }
-    }
-    for (let i = 0; i < select.length; i++) {
-        if (select[i].value === "") {
-            select[i].style.borderColor = "red";
-            isGood = false;
-        }
-    }
-    if (isGood === false) {
-        alert("Niste popunili sva polja");
+
+    let regex = new RegExp("^[A-Z][^<>#$]*[.!?]$");
+
+    if (textareas.value === ""){
         event.preventDefault();
-        focusEmptyForm(poruka);
-        // spriječavamo slanje forme
-        
+        textareas.style.borderColor = "red";
+        poruka = "Textarea ne smije biti prazan";
+        focusEmptyForm(poruka, textareas);
+        isGood = false;
+        return isGood;
+    }
+
+    let textareasValue = textareas.value.trim();
+    let recenice = textareasValue.split(/[\.\!\?]/);
+
+    for(let i = 0; i < recenice.length; i++){
+        recenice[i] = recenice[i].trim();
+        if(recenice[i] !== "" && i !== recenice.length - 1){
+            recenice[i] += textareasValue.charAt(textareasValue.indexOf(recenice[i]) + recenice[i].length);
+        }
+    }
+
+    for(let i = 0; i < recenice.length; i++){
+        if(recenice[i] === ""){
+            recenice.splice(i, 1);
+        }
+    }
+
+    if(recenice.length > 4){
+        event.preventDefault();
+        textareas.style.borderColor = "red";
+        poruka = "Textarea ima više od 4 rečenice";
+        focusEmptyForm(poruka, textareas);
+        isGood = false;
+        return isGood;
+    }
+
+    for(let i = 0; i < recenice.length; i++){
+        if (!regex.test(recenice[i])) {
+            event.preventDefault();
+            textareas.style.borderColor = "red";
+            poruka = "Svaka rečenica mora započeti velikim slovom i završavati točkom, upitnikom ili uskličnikom te ne smije sadržavati specijalne znakove";
+            focusEmptyForm(poruka, textareas);
+            isGood = false;
+            return isGood;
+        }
+    }
+    
+    if (selects.value === "") {
+        event.preventDefault();
+        selects.style.borderColor = "red";
+        poruka = "Potrebno je odabrati barem jednu opciju";
+        focusEmptyForm(poruka, selects);
+        isGood = false;
+        return isGood;
+    }
+
+    if (!isGood) {
+        alert("Niste popunili sva polja");
+        event.preventDefault();        
         return false;
     }
     else{
@@ -100,8 +144,8 @@ function findElementLocation(element) {
     while (currentElement) {
         top += currentElement.offsetTop;
         left += currentElement.offsetLeft;
+        currentElement = currentElement.offsetParent;
     }
-
     return { top, left };
 }
 
@@ -126,8 +170,7 @@ function focusEmptyForm(message, element){
 
         emptyField.parentNode.appendChild(modalMessage);
 
-        modalMessage = "<div class='modalMessage'><p>Potrebno je odabrati opciju</p></div>";
-        emptyField.innerHTML += modalMessage;
+        // emptyField.innerHTML += modalMessage;
 }
 
 document.addEventListener("keydown", (e) => {
@@ -135,28 +178,54 @@ document.addEventListener("keydown", (e) => {
     for(let i = 0; i < inputs.length; i++){
         if(inputs[i].value === ""){
             inputs[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
-            textareas[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
+        }
+    }
+    for(let i = 0; i < selects.length; i++){
+        if(selects[i].value === ""){
             selects[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
         }
     }
+    for(let i = 0; i < textareas.length; i++){
+        if(textareas[i].value === ""){
+            textareas[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
+        }
+    }
 });
+
 document.addEventListener("reset", (e) => {
     deleteAllModalMessages();
     for(let i = 0; i < inputs.length; i++){
         if(inputs[i].value === ""){
             inputs[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
-            textareas[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
+        }
+    }
+    for(let i = 0; i < selects.length; i++){
+        if(selects[i].value === ""){
             selects[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
         }
     }
+    for(let i = 0; i < textareas.length; i++){
+        if(textareas[i].value === ""){
+            textareas[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
+        }
+    }
 });
+
 form.addEventListener("change", (e) => {
     deleteAllModalMessages();
     for(let i = 0; i < inputs.length; i++){
         if(inputs[i].value === ""){
             inputs[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
-            textareas[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
+        }
+    }
+    for(let i = 0; i < selects.length; i++){
+        if(selects[i].value === ""){
             selects[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
+        }
+    }
+    for(let i = 0; i < textareas.length; i++){
+        if(textareas[i].value === ""){
+            textareas[i].style.border = "1px solid rgba(0, 0, 0, 0.4)";
         }
     }
 });
